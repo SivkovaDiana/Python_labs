fe0e2a5879613ad67e80e4b943ef37faaa94c019
Diana <diana.sivkova18av@gmail.com>
2022-12-13 22:53:41 +0500

    the sum of random numbers

--- lab2/main.py
+++ lab2/main.py
@@ -1,7 +1,7 @@
 from pyDatalog import pyDatalog
 import random
 
-pyDatalog.create_terms('X, N, Sum, Average')
+pyDatalog.create_terms('X, N, Sum, Average, rand_sum, result, Z')
 
 N=888888
 Sum[X] = ((1+X)*X)/2
@@ -9,3 +9,7 @@ print(Sum[N] == Sum)
 
 Average[X] = (X+1)/2
 print(Average[N] == Average)
+
+rand_numbers = [random.choice(range(N)) for i in range(100)]
+(result["rand_sum"] == sum_(Z, for_each=Z)) <= Z.in_(rand_numbers)
+print(result["rand_sum"] == X)
