da8ac6ee1d4b2d212c0920edd196a49f838cc4fc
Diana <diana.sivkova18av@gmail.com>
2022-12-13 22:57:02 +0500

    findeing the median of random numbers

--- lab2/main.py
+++ lab2/main.py
@@ -13,3 +13,5 @@ print(Average[N] == Average)
 rand_numbers = [random.choice(range(N)) for i in range(100)]
 (result["rand_sum"] == sum_(Z, for_each=Z)) <= Z.in_(rand_numbers)
 print(result["rand_sum"] == X)
+
+print("mediana: ", (rand_numbers[49]+rand_numbers[50])/2)
