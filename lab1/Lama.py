7f1e069bc2e0d0d1bd1918f305ae62424efc8f34
Diana <diana.sivkova18av@gmail.com>
2022-12-13 16:26:58 +0500

    creation of the fifth class

---
+++ lab1/Lama.py
@@ -0,0 +1,9 @@
+form Animal import Animal
+class Lama(Animal):
+    def __init__(self, continent, color, max_speed, symbol):
+        super().__init__(continent, color)
+        self.name = "Lama"
+        self._max_speed = max_speed
+        self._symbol = symbol
+    def __repr__(self):
+        return f'Animal(name = {self.name}, continent = {self._continent}, color = {self._color}, max_speed = {self._max_speed}, symbol = {self._symbol}'
