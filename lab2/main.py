955cbf43883a042d0d8e000dbf9c026bc4951c69
Diana <diana.sivkova18av@gmail.com>
2022-12-13 21:43:20 +0500

    finding the average value

--- lab2/main.py
+++ lab2/main.py
@@ -1,8 +1,11 @@
 from pyDatalog import pyDatalog
 import random
 
-pyDatalog.create_terms('X, N, Sum')
+pyDatalog.create_terms('X, N, Sum, Average')
 
 N=888888
 Sum[X] = ((1+X)*X)/2
 print(Sum[N] == Sum)
+
+Average[X] = (X+1)/2
+print(Average[N] == Average)
