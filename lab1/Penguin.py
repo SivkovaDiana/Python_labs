d5f0a4d86bf086e01084158770fb7aec7883ebc3
SivkovaDiana <92534589+SivkovaDiana@users.noreply.github.com>
2022-12-14 10:32:30 +0500

    Update Penguin.py

--- lab1/Penguin.py
+++ lab1/Penguin.py
@@ -1,9 +1,15 @@
 from Animal import Animal
 class Penguin(Animal):
-    def __init__(self, continent, color, max_speed, food):
+    def __init__(self, continent, color, max_speed, food, _name):
         super().__init__(continent, color)
-        self.name = "Penguin"
+        self.name = _name
         self._max_speed = max_speed
         self._food = food
+        
     def __repr__(self):
-        return f'Animal(name = {self.name}, continent = {self._continent}, color = {self._color}, max_speed = {self._max_speed}, food = {self._food}'
+        return f'Animal(_name="{self.name}", continent = "{self._continent}", color = "{self._color}", max_speed = {self._max_speed}, food = "{self._food}"'
+   
+if __name__ == "__main__":
+    a_penguin = Penguin(...)
+    print(a_penguin)
+    
