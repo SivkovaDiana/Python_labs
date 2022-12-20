d4e9471e9bafd4143f5cd2fee5bd541322684b3b
SivkovaDiana <92534589+SivkovaDiana@users.noreply.github.com>
2022-12-21 02:54:31 +0500

    Update Bear.py

--- lab1/Bear.py
+++ lab1/Bear.py
@@ -1,9 +1,9 @@
 from Animal import Animal
 class Bear(Animal):
-    def __init__(self, continent, color, max_speed, life_expectancy):
+    def __init__(self, continent, color, max_speed, life_expectancy, _name):
         super().__init__(continent, color)
-        self.name = "Bear"
+        self.name = _name
         self._max_speed = max_speed
         self._life_expectancy = life_expectancy
     def __repr__(self):
-        return f'Animal(name = {self.name}, continent = {self._continent}, color = {self._color}, max_speed = {self._max_speed}, life_expectancy = {self._life_expectancy}'
+        return f'Animal(_name="{self.name}", continent="{self._continent}", color="{self._color}", max_speed={self._max_speed}, life_expectancy={self._life_expectancy}'
