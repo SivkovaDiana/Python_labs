188d0260b1f75386a06bbc52793c98b6f444a2fb
SivkovaDiana <92534589+SivkovaDiana@users.noreply.github.com>
2022-12-21 02:57:32 +0500

    Update Lama.py

--- lab1/Lama.py
+++ lab1/Lama.py
@@ -1,9 +1,10 @@
 from Animal import Animal
 class Lama(Animal):
-    def __init__(self, continent, color, max_speed, symbol):
+    def __init__(self, continent, color, max_speed, symbol, _name):
         super().__init__(continent, color)
-        self.name = "Lama"
+        self.name = _name
         self._max_speed = max_speed
         self._symbol = symbol
+        
     def __repr__(self):
-        return f'Animal(name = {self.name}, continent = {self._continent}, color = {self._color}, max_speed = {self._max_speed}, symbol = {self._symbol}'
+        return f'Animal(_name="{self.name}", continent="{self._continent}", color="{self._color}", max_speed={self._max_speed}, symbol="{self._symbol}"'
