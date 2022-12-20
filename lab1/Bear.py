6b90b873e5a4bcc1bef20c279eff735e88c23b2d
SivkovaDiana <92534589+SivkovaDiana@users.noreply.github.com>
2022-12-21 02:56:30 +0500

    Update Bear.py

--- lab1/Bear.py
+++ lab1/Bear.py
@@ -5,5 +5,6 @@ class Bear(Animal):
         self.name = _name
         self._max_speed = max_speed
         self._life_expectancy = life_expectancy
+        
     def __repr__(self):
         return f'Animal(_name="{self.name}", continent="{self._continent}", color="{self._color}", max_speed={self._max_speed}, life_expectancy={self._life_expectancy}'
