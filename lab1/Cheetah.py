8d4d3d5bb342d60fe60d21127fafccad42bcbe85
SivkovaDiana <92534589+SivkovaDiana@users.noreply.github.com>
2022-12-21 02:56:16 +0500

    Update Cheetah.py

--- lab1/Cheetah.py
+++ lab1/Cheetah.py
@@ -1,9 +1,10 @@
 from Animal import Animal
 class Cheetah(Animal):
-    def __init__(self, continent, color, max_speed, weight):
+    def __init__(self, continent, color, max_speed, weight, _name):
         super().__init__(continent, color)
-        self.name = "Cheetah"
+        self.name = _name
         self._max_speed = max_speed
         self._weight = weight
+        
     def __repr__(self):
-        return f'Animal(name = {self.name}, continent = {self._continent}, color = {self._color}, max_speed = {self._max_speed}, weight = {self._weight}'
+        return f'Animal(_name="{self.name}", continent="{self._continent}", color= "{self._color}", max_speed={self._max_speed}, weight={self._weight}'
